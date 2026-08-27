ed982f271c01a2ea27f635af0d2e19967ad3fd3f
abhayminer01 <abhayvapvl@gmail.com>
2026-08-27 15:10:25 +0530

    Rework on grid  mechanism and apple generation

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,15 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
+    int appleX;
+    int appleY;
+    Random random;
 
     GamePanel() {
+        random = new Random();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.black);
         this.setFocusable(true);
+        startGame();
+    }
+
+    void startGame() {
+        generateApple();
     }
 
     public void paintComponent(Graphics g) {
@@ -19,8 +29,16 @@ public class GamePanel extends JPanel {
 
     public void draw(Graphics g) {
         for (int i = 0; i <= UNIT_SIZE; i++) {
-            g.drawLine(0, (int)(SCREEN_HEIGHT/UNIT_SIZE)*i, SCREEN_HEIGHT, (int)(SCREEN_HEIGHT/UNIT_SIZE)*i);
-            g.drawLine((int)(SCREEN_WIDTH/UNIT_SIZE)*i, 0, (int)(SCREEN_WIDTH/UNIT_SIZE)*i, SCREEN_WIDTH);
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
         }
+
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+    }
+
+    public void generateApple() {
+        appleX = (random.nextInt((int)SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = (random.nextInt((int)SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
 }
